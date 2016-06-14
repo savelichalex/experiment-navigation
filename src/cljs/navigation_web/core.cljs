94bc9cc7a5ecebe28ab41e496683e96954628fe6
savelichalex <savelichalex93@gmail.com>
2016-06-15 00:05:57 +0300

    Add concept of navigation

--- src/cljs/navigation_web/core.cljs
+++ src/cljs/navigation_web/core.cljs
@@ -4,6 +4,8 @@
               [secretary.core :as secretary :include-macros true]
               [accountant.core :as accountant]))
 
+(enable-console-print!)
+
 ;; -------------------------
 ;; Views
 
@@ -14,17 +16,19 @@
                  :left 0
                  :right 0
                  :opacity opacity
+                 :z-index 1
                  :background-color "blue"}}])
 
 (defn first-scene-content [left]
-  [:div {:style {:display "absolute"
+  [:div {:style {:position "absolute"
+                 :display "flex"
                  :top 0
                  :left left
                  :right 0
                  :bottom 0
                  :padding-bottom "30px"
                  :justify-content "center"
-                 :background-color "white"
+                 :background-color "grey"
                  :align-items "center"}}
    [:span {:style {:flex 1}} "HDGJHGDJ"]])
 
@@ -44,10 +48,12 @@
                  :left 0
                  :right 0
                  :opacity opacity
+                 :z-index 1
                  :background-color "yellow"}}])
 
 (defn second-scene-content [left]
-  [:div {:style {:display "absolute"
+  [:div {:style {:position "absolute"
+                 :display "flex"
                  :top 0
                  :left left
                  :right 0
@@ -67,17 +73,6 @@
                  :border "1px solid black"
                  :background-color "pink"}}])
 
-(defn second-scene [{:keys [width left]}]
-  [:div {:style {:display "flex"
-                 :flex-direction "column"
-                 :position "relative"
-                 :height "300px"
-                 :width "150px"
-                 :background-color "white"}}
-   [second-scene-navbar {:left 0 :width 150}]
-   [second-scene-content]
-   [second-scene-tabbar]])
-
 (def first-s {:navbar first-scene-navbar
               :content first-scene-content
               :tabbar first-scene-tabbar})
@@ -87,7 +82,7 @@
                :tabbar second-scene-tabbar})
 
 (defn navbar-transition [first-navbar second-navbar [width height progress]]
-  (if (progress < 0.5)
+  (if (< progress 0.5)
     [first-navbar (- 1 (* progress 2))]
     [second-navbar (- (* progress 2) 1)]))
 
@@ -101,7 +96,7 @@
 
 (defn tabbar-transition [first-tabbar second-tabbar [width height progress]]
   (let [tabbar-height 30]
-    (if (progress < 0.5)
+    (if (< progress 0.5)
       [first-tabbar
        tabbar-height
        (- tabbar-height (* (- 1 (* progress 2)) tabbar-height))]
@@ -113,14 +108,36 @@
                       :content content-transition
                       :tabbar tabbar-transition})
 
+(defn render-scenes [wrapper first-scene second-scene relation props]
+  (into wrapper
+        (for [[key _] first-scene]
+          (apply
+            (get relation key)
+            [(get first-scene key)
+             (get second-scene key)
+             props]))))
+
 (defn home-page []
   (let [width 150
         height 300
-        progress (reagent/atom 0)]
+        progress (reagent/atom 0)
+        render-loop (fn []
+                      (when (< @progress 1)
+                        (js/requestAnimationFrame #(do
+                                                    (swap! progress (fn [a] (+ a 0.01)))))))]
     (fn []
-      [:div {:style {:position "relative"}}
-       [first-scene {:width width :height height :progress @progress}]
-       [second-scene {:width width :height height :progress @progress}]])))
+      (render-loop)
+      (render-scenes
+        [:div {:style {:position "relative"
+                       :width (str width "px")
+                       :height (str height "px")
+                       :overflow "hidden"}}]
+        first-s
+        second-s
+        first-to-second
+        [width height @progress])
+      ;(apply navbar-transition [first-scene-navbar second-scene-navbar [width height @progress]])
+      )))
 
 (defn about-page []
   [:div [:h2 "About navigation-web"]
